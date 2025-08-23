2e35504dcc178bb0b92261ec9aeaaaf9e8ec044e
Hong Gayeon <157695332+gayeoniee@users.noreply.github.com>
2025-08-23 09:09:49 +0900

    prompt

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 import meilisearch
-client = meilisearch.Client('http://127.0.0.1:7700', '14_MRs3BW8jO62eowQOL9Io9drAMPVtX0wA3NlpRghY')
+client = meilisearch.Client('http://127.0.0.1:7700', MEILISEARCH_MASTER_KEY)
 
 def stock_search(query):    
     return client.index('nasdaq').search(query)
