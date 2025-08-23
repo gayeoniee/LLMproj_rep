06193b0814947354b811fcb54411cf1f7fbef57d
Hong Gayeon <157695332+gayeoniee@users.noreply.github.com>
2025-08-23 09:11:47 +0900

    prompt

--- search.py
+++ search.py
@@ -1,5 +1,14 @@
 import meilisearch
-client = meilisearch.Client('http://127.0.0.1:7700', MEILISEARCH_MASTER_KEY)
+rom dotenv import load_dotenv
+import os
+load_dotenv()
+meilisearch_master_key = os.getenv("MEILISEARCH_MASTER_KEY")
+if meilisearch_master_key:
+    os.environ["MEILISEARCH_MASTER_KEY"] = meilisearch_master_key
+print("MEILISEARCH Key configured:", "MEILISEARCH_MASTER_KEY" in os.environ)
+
+
+client = meilisearch.Client('http://127.0.0.1:7700', meilisearch_master_key)
 
 def stock_search(query):    
     return client.index('nasdaq').search(query)
